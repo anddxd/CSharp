e53b56ef0e0b9cc144187d44692c47575dc2d7b2
Andrey M. Medeiros Pinto <anddxd@hotmail.com>
2018-01-26 16:02:17 -0300

    Adição de projeto com exemplo de thread do tipo background e foreground

---
+++ Exam70483/BackgroundThread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace BackgroundThread
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            // Quando a thread é background o CLR é executado imediatamente, fechando a aplicação. 
+            // Quando a thread é foreground toda a thread é executada antes do CLR ser executado.
+            t.IsBackground = false;
+            t.Start();
+        }
+    }
+}
